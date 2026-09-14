dafa5586086ca684fd04fcd782d752dd8172d60b
Rui <ruiconti@gmail.com>
2026-09-14 17:51:35 -0400

    fix(eve): defer scheduled delivery until background results (#3350)

--- packages/eve/src/harness/background-tools.ts
+++ packages/eve/src/harness/background-tools.ts
@@ -33,6 +33,7 @@ export interface BackgroundToolCallBatch {
 }
 
 export interface BackgroundToolExecutor {
+  hasPendingTasks?(): boolean;
   readAgentViews?(): Promise<readonly AgentView[]>;
   execute(input: {
     readonly batch: BackgroundToolCallBatch;
